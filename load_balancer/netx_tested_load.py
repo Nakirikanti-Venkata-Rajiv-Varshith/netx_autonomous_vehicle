cf9e51a83910468d2d633ed5e0038759c983083b
Nakirikanti-Venkata-Rajiv-Varshith <omenforvar@gmail.com>
2026-06-04 14:27:06 +0530

    sending working alb lets hope

--- load_balancer/netx_tested_load.py
+++ load_balancer/netx_tested_load.py
@@ -648,35 +648,8 @@ class LoadBalancerNode:
         )
         latency_penalty = clamp((rtt_penalty * 0.75) + (jitter_penalty * 0.25))
 
-      
-
-        # edge_score = clamp((low_latency * 0.35) + (low_compute_load * 0.20) + (low_motion * 0.17) + (power_saving * 0.2))
-        # cloud_score = clamp((high_accuracy_need * 0.40) + (bandwidth_quality * 0.40) - (latency_penalty * 0.20) + (profile["scene_complexity"] * 0.25))
-
-        resource_pressure = max(cpu_norm, gpu_norm, ram_norm)
-
-        overload_bonus = 0.0
-
-        if resource_pressure > 0.85:
-            overload_bonus = (resource_pressure - 0.85) / 0.15
-
-        edge_score = clamp(
-            (low_latency * 0.35)
-            + (low_compute_load * 0.20)
-            + (low_motion * 0.17)
-            + (power_saving * 0.2)
-        )
-
-        cloud_score = clamp(
-            (high_accuracy_need * 0.40)
-            + (bandwidth_quality * 0.40)
-            - (latency_penalty * 0.20)
-            + (profile["scene_complexity"] * 0.15)
-        )
-
-        cloud_score = clamp(
-            cloud_score + overload_bonus * 0.15
-        )
+        edge_score = clamp((low_latency * 0.35) + (low_compute_load * 0.20) + (low_motion * 0.17) + (power_saving * 0.2))
+        cloud_score = clamp((high_accuracy_need * 0.40) + (bandwidth_quality * 0.40) - (latency_penalty * 0.20) + (profile["scene_complexity"] * 0.25))
 
         if latency_critical:
             edge_score = clamp(edge_score + 0.2)
@@ -707,7 +680,7 @@ class LoadBalancerNode:
             else:
                 route = "onboard"
 
-        # route = "onboard"
+        # route = "offboard"
 
 
         rospy.logdebug(
