03d769401ebac326e8c982240e060acf6aba87d7
Nakirikanti-Venkata-Rajiv-Varshith <omenforvar@gmail.com>
2026-06-08 11:18:11 +0530

    using if lese skip

--- keep_log_of_files_alb/07_06_ml_fusion_score.py
+++ keep_log_of_files_alb/07_06_ml_fusion_score.py
@@ -814,7 +814,7 @@ class LoadBalancerNode:
 
 
 
-                            ####*************************** ML FUSION ROUTE STYLE *********************#########
+                            ####*************************** ML FUSION ROUTE STYLE *********************#####
         # =====================================================
 # ROUTING SCORE
 # =====================================================
@@ -858,8 +858,8 @@ class LoadBalancerNode:
                 0.063 * change_norm
             )
 
-            # # App-specific learned bias
-            # routing_score += APP_OFFLOAD_BIAS.get(app, 0.0)
+            # App-specific learned bias
+            routing_score += APP_OFFLOAD_BIAS.get(app, 0.0)
 
             # # ======================================================
             # # Resource Pressure Bonus
