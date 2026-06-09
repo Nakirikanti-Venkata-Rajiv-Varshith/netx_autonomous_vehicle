fb7ee18e08d3ff218b904cc434b1248f3ad3c7fd
Nakirikanti-Venkata-Rajiv-Varshith <omenforvar@gmail.com>
2026-06-09 11:47:55 +0530

    senidng the offboard to get the test results

--- spike/new_gradual_spike.py
+++ spike/new_gradual_spike.py
@@ -18,7 +18,7 @@ except:
 
 LEVELS = list(range(20, 100, 5))
 STEP_TIME = 5
-HOLD_TIME = 60
+HOLD_TIME = 80
 
 
 # ============================================================
@@ -115,6 +115,31 @@ class GPUStressor:
         self.running = False
 
 
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# TIMER 
+
+
+    def timer_worker(self):
+
+        start_time = time.time()
+
+        while not self.stop_event.is_set():
+
+            elapsed = int(time.time() - start_time)
+
+            hrs = elapsed // 3600
+            mins = (elapsed % 3600) // 60
+            secs = elapsed % 60
+
+            print(
+                f"[TIMER] {hrs:02d}:{mins:02d}:{secs:02d}"
+            )
+
+            time.sleep(1)
+
+
+
+
 # ============================================================
 # MAIN
 # ============================================================
@@ -122,6 +147,8 @@ class GPUStressor:
 class StressController:
 
     def __init__(self):
+
+        self.timer_thread = None
 
         self.stop_event = mp.Event()
 
@@ -235,6 +262,12 @@ class StressController:
 
     def run(self):
 
+        self.timer_thread = threading.Thread(
+            target=self.timer_worker,
+            daemon=True
+        )
+
+        self.timer_thread.start()        
         self.start_cpu()
 
         self.start_gpu()
